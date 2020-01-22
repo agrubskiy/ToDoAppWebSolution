ba9f091cbcbe578778081f625e86438ce5cc7641
Anton Grubskiy <masthief@outlook.com>
2020-01-22 21:19:48 +0200

    First Commit

---
+++ ToDoAppWebSolution/Models/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAppWebSolution.Models
+{
+	public class ToDoItem
+	{
+		[Required]
+		public string Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Notes { get; set; }
+
+		public bool Done { get; set; }
+	}
+}
